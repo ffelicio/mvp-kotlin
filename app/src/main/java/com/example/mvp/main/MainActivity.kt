3d0bd5816859500b2c5abf9e3fd14fd88baf162a
Fernando da Costa Felício <ffelicio@autodoc.com.br>
2018-09-11 16:51:15 -0300

    Finalização do recurso de padronização com MVP

--- app/src/main/java/com/example/mvp/main/MainActivity.kt
+++ app/src/main/java/com/example/mvp/main/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.mvp.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvp.R
